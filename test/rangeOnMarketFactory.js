11c663e2d7e3cd44c739865919f153f1afd6ad98
Michal Graczykowski <graczykowskimichal@gmail.com>
2017-09-08 23:22:20 +0200

    Test updated

--- test/rangeOnMarketFactory.js
+++ test/rangeOnMarketFactory.js
@@ -1,26 +1,55 @@
 var RangeOnMarketFactory = artifacts.require("./RangeOnMarketFactory.sol");
+var BigNumber = require('bignumber.js');
+var Web3Utils = require('web3-utils');
 
 contract('RangeOnMarketFactory', function(accounts) {
-    it("return 0 for a not set key", function() {
-        const notSetKey = 0x1000000000000000000000000000000000000000000000000000000000000000;
-        const zero = 0x0000000000000000000000000000000000000000000000000000000000000000;
-        return RangeOnMarketFactory.deployed().then(function(instance) {
-            return instance.get(notSetKey);
-        }).then(function(value) {
-            assert.equal(value, zero, "get(notSetKey) did not equal zero");
+
+    var contract;
+
+    beforeEach(function(done) {
+        RangeOnMarketFactory.new().then(function(instance) {
+            contract = instance;
+            done();
+        })
+    });
+
+    it("should return 0 for a not set key", function(done) {
+        var notSetKey = 555;
+
+        contract.get(notSetKey).then(function(value) {
+            assert(new BigNumber(value).equals(0));
+            done();
         });
     });
 
-    it("return value previously set for key", function() {
-        const key = 0x1000000000000000000000000000000000000000000000000000000000000000;
-        const value = 0x2000000000000000000000000000000000000000000000000000000000000000;
-        return RangeOnMarketFactory.deployed().then(function(instance) {
-            instance.set(key, value);
-            return instance;
-        }).then(function(instance) {
-            return instance.get(key);
-        }).then(function(returnedValue) {
-            assert.equal(returnedValue, value, "returned value did not equal value set to key");
+    it("should return value previously set for key", function(done) {
+        var key = 'key';
+        var value = 'value';
+
+        contract.set(key, value).then(function() {
+            return contract.get(key);
+        }).then(function (returnedValue) {
+            assert.equal(Web3Utils.hexToUtf8(returnedValue), value);
+            done();
+        });
+    });
+
+    it("should return all key-value pairs", function(done) {
+        var key1 = 'key1';
+        var value1 = 'value1';
+        var key2 = 'key2';
+        var value2 = 'value2';
+
+        contract.set(key1, value1).then(function() {
+            return contract.set(key2, value2);
+        }).then(function() {
+            return contract.getAll();
+        }).then(function(all) {
+            assert.equal(Web3Utils.hexToUtf8(all[0]), key1);
+            assert.equal(Web3Utils.hexToUtf8(all[1]), value1);
+            assert.equal(Web3Utils.hexToUtf8(all[2]), key2);
+            assert.equal(Web3Utils.hexToUtf8(all[3]), value2);
+            done();
         });
     });
 });
